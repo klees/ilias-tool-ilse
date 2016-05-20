34c3e6943e334bf2eca34871cda4171891f5644f
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-05-20 12:05:27 +0200

    add primary keys and change modes

--- src/Config/Table.php
+++ src/Config/Table.php
@@ -7,22 +7,27 @@ namespace CaT\InstILIAS\Config;
  * @method string name()
  * @method string mode()
  * @method array columns()
+ * @method array primaryKeys()
  */
 class Table extends Base {
+	const NAME_REGEX = "/^[a-z][_a-z0-9]*/";
+
 	/**
 	 * @inheritdocs
 	 */
 	public static function fields() {
 		return array
-			( "name"	=> array("string", false)
-			, "mode"	=> array("string", false)
-			, "columns"	=> array(array("\\CaT\\InstILIAS\\Config\\TableColumn"), false)
+			( "name"			=> array("string", false)
+			, "mode"			=> array("string", false)
+			, "columns"			=> array(array("\\CaT\\InstILIAS\\Config\\TableColumn"), false)
+			, "primary_keys"	=> array(array("string"), true)
 			);
 	}
 
 	protected static $valid_modes = array(
 		"create",
-		"update",
+		"addColumn",
+		"dropColumn",
 		"clear",
 		"drop"
 	);
@@ -32,6 +37,8 @@ class Table extends Base {
 	 */
 	protected function checkValueContent($key, $value) {
 		switch($key) {
+			case "name":
+				return $this->checkContentPregmatch($value, self::NAME_REGEX);
 			case "mode":
 				return $this->checkContentValueInArray($value, self::$valid_modes);
 			default:
