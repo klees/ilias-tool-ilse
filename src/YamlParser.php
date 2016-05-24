2106b9a2bed92b9a0bcd29ee6bb200f1be711b75
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-05-24 11:27:04 +0200

    show path to missed config value

--- src/YamlParser.php
+++ src/YamlParser.php
@@ -13,26 +13,27 @@ class YamlParser implements \CaT\InstILIAS\interfaces\Parser {
 		return $this->createConfig($yaml, $class);
 	}
 
-	protected function createConfig($yaml, $class) {
+	protected function createConfig($yaml, $class, $path = "") {
 		$vals = array();
 		foreach ($class::fields() as $key => $type) {
 			$type_val = $type[0];
 			$optional = $type[1];
 
 			if(is_subclass_of($type_val, "\\CaT\\InstILIAS\\Config\\Base") ) {
-				$value = $this->yamlValue($yaml,$key,$optional);
+				$value = $this->yamlValue($yaml, $key, $path, $optional);
 
 				if(!$optional || ($optional && $value !== null)) {
-					$vals[] = $this->createConfig($value, $type_val);
+					$new_path = ($path == "") ? $key : $path.":".$key;
+					$vals[] = $this->createConfig($value, $type_val, $new_path);
 				} else {
 					$vals[] = null;
 				}
 			}
 			else if ($type_val == "string") {
-				$vals[] = $this->yamlValue($yaml,$key,$optional, "");
+				$vals[] = $this->yamlValue($yaml, $key, $path, $optional, "");
 			}
 			else if ($type_val == "int") {
-				$vals[] = $this->yamlValue($yaml,$key,$optional,0);
+				$vals[] = $this->yamlValue($yaml, $key, $path, $optional, 0);
 			}
 			else if(is_array($type_val)) {
 				assert('count($type_val) === 1');
@@ -40,12 +41,14 @@ class YamlParser implements \CaT\InstILIAS\interfaces\Parser {
 				
 				if(is_subclass_of($content, "\\CaT\\InstILIAS\\Config\\Base")) {
 					$sub_vals = array();
-					foreach ($this->yamlValue($yaml,$key,$optional,array()) as $key => $value) {
-						$sub_vals[] = $this->createConfig($value, $content);
+					$new_path = ($path == "") ? $key : $path.":".$key;
+					foreach ($this->yamlValue($yaml, $key, $new_path, $optional, array()) as $key => $value) {
+						$new_path2 = ($new_path == "") ? $key : $new_path.":".$key;
+						$sub_vals[] = $this->createConfig($value, $content, $new_path2);
 					}
 					$vals[] = $sub_vals;
 				} else {
-					$vals[] = $this->yamlValue($yaml,$key,$optional,array());
+					$vals[] = $this->yamlValue($yaml, $key, $path, $optional, array());
 				}
 			}
 			else {
@@ -57,11 +60,11 @@ class YamlParser implements \CaT\InstILIAS\interfaces\Parser {
 		return $class_handle->newInstanceArgs($vals);
 	}
 
-	protected function yamlValue($yaml, $key, $optional, $baseValue = null) {
+	protected function yamlValue($yaml, $key, $path, $optional, $baseValue = null) {
 		if(!array_key_exists($key, $yaml) && $optional) {
 			return $baseValue;
 		} else if(!array_key_exists($key, $yaml) && !$optional) {
-			echo "Required configuration entry \"".$key."\" was not found. Please check your config.yaml";
+			echo "Required configuration entry \"".$path.":".$key."\" was not found. Please check your config.yaml";
 			die(1);
 		}
 
