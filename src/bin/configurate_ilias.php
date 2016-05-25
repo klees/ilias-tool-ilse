0ac74dcc3025b9d746e82bff9908bc59fadee5df
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-05-25 08:32:22 +0200

    restore configurate_ilias.php

---
+++ src/bin/configurate_ilias.php
@@ -0,0 +1,42 @@
+<?php
+$config_path = $argv[1];
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+error_reporting(E_ALL & ~E_STRICT & ~E_DEPRECATED);
+
+$yaml_string = file_get_contents($config_path);
+$parser = new \CaT\InstILIAS\YamlParser();
+$general_config = $parser->read_config($yaml_string, "\\CaT\\InstILIAS\\Config\\General");
+
+$absolute_path = $general_config->server()->absolutePath();
+$client_id = $general_config->client()->name();
+
+echo "\n\nConfigurate ILIAS.";
+$ilias_configurator = new \CaT\InstILIAS\IliasReleaseConfigurator($absolute_path, $client_id);
+
+if($general_config->category() !== null) {
+	echo "\nCreate Categories.";
+	$ilias_configurator->createCategories($general_config->category());
+	echo "\t\tDone...\n";
+}
+
+if($general_config->orgunit() !== null) {
+	echo "\nCreate OrgUnits.";
+	$ilias_configurator->createOrgUnits($general_config->orgunit());
+	echo "\t\tDone...\n";
+}
+
+if($general_config->role() !== null) {
+	echo "\nCreate global Roles.";
+	$ilias_configurator->createRoles($general_config->role());
+	echo "\t\tDone...\n";
+}
+
+if($general_config->ldap() !== null) {
+	echo "\nConfigure LDAP Server.";
+	$ilias_configurator->configureLDAPServer($general_config->ldap());
+	echo "\t\tDone...\n";
+}
+
+echo "\n\nIlias successfull configured.";
