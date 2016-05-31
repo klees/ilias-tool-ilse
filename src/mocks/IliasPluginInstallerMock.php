9be576ea0b559df60e2769c2820f1ba18fe421b9
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-05-31 15:17:55 +0200

    add plugin installer mock for php unit interface test

---
+++ src/mocks/IliasPluginInstallerMock.php
@@ -0,0 +1,45 @@
+<?php
+/* Copyright (c) 2016 Stefan Hecken <stefan.hecken@concepts-and-training.de>, Extended GPL, see LICENSE */
+
+namespace CaT\InstILIAS\mocks;
+/**
+ * implementation of plugin interface to install plugins
+ *
+ * @author Stefan Hecken <stefan.hecken@concepts-and-training.de>
+ */
+class IliasPluginInstallerMock implements \CaT\InstILIAS\interfaces\Plugin {
+	/**
+	 * @inheritdoc
+	 */
+	public function install(\CaT\InstILIAS\Config\Plugin $plugin, $absolute_path) {
+		return true;
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function update(\CaT\InstILIAS\Config\Plugin $plugin) {
+		return true;
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function activate(\CaT\InstILIAS\Config\Plugin $plugin) {
+		return true;
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function deactivate(\CaT\InstILIAS\Config\Plugin $plugin) {
+		return true;
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function updateLanguage(\CaT\InstILIAS\Config\Plugin $plugin) {
+		return;
+	}
+}
