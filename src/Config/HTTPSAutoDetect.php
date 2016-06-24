7114397b26d43782fe82164ee2d885d3737622ea
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-06-24 09:38:35 +0200

    add https auto detect config and test

---
+++ src/Config/HTTPSAutoDetect.php
@@ -0,0 +1,45 @@
+<?php
+/* Copyright (c) 2016 Stefan Hecken <stefan.hecken@concepts-and-training.de>, Extended GPL, see LICENSE */
+
+namespace CaT\InstILIAS\Config;
+
+/**
+ * Configuration for the HTTPS Auto Detect ILIAS runs on.
+ *
+ * @author Stefan Hecken <stefan.hecken@concepts-and-training.de>
+ *
+ * @method string enabled()
+ * @method string headerName()
+ * @method string headerValue()
+ */
+class HTTPSAutoDetect extends Base {
+	const URL_REG_EX = "/^^(https:\/\/|http:\/\/)/";
+
+	/**
+	 * @inheritdocs
+	 */
+	public static function fields() {
+		return array
+			( "enabled"		=> array("int", false)
+			, "header_name"	=> array("string", true)
+			, "header_value"	=> array("string", true)
+			);
+	}
+
+	public static $valid_enables = array(
+			0
+			,1
+		);
+
+	/**
+	 * @inheritdocs
+	 */
+	protected function checkValueContent($key, $value) {
+		switch($key) {
+			case "enabled":
+				return $this->checkContentValueInArray($value, self::$valid_enables);
+			default:
+				return parent::checkValueContent($key, $value);
+		}
+	}
+}
---
+++ tests/HTTPSAutoDetectConfigTest.php
@@ -0,0 +1,62 @@
+<?php
+
+use \CaT\InstILIAS\Config\HTTPSAutoDetect;
+
+class HTTPSAutoDetectConfigTest extends PHPUnit_Framework_TestCase{
+	public function test_not_enough_params() {
+		try {
+			$config = new HTTPSAutoDetect();
+			$this->assertFalse("Should have raised.");
+		}
+		catch (\InvalidArgumentException $e) {}
+	}
+
+	/**
+	 * @dataProvider	HTTPSAutoDetectValueProvider
+	 */
+	public function test_HTTPSAutoDetectConfig($enabled, $header_name, $header_value, $valid) {
+		if ($valid) {
+			$this->_test_valid_HTTPSAutoDetectConfig($enabled, $header_name, $header_value);
+		}
+		else {
+			$this->_test_invalid_HTTPSAutoDetectConfig($enabled, $header_name, $header_value);
+		}
+	}
+
+	public function _test_valid_HTTPSAutoDetectConfig($enabled, $header_name, $header_value) {
+		$config = new HTTPSAutoDetect($enabled, $header_name, $header_value);
+		$this->assertEquals($enabled, $config->enabled());
+		$this->assertEquals($header_name, $config->headerName());
+		$this->assertEquals($header_value, $config->headerValue());
+	}
+
+	public function _test_invalid_HTTPSAutoDetectConfig($enabled, $header_name, $header_value) {
+		try {
+			$config = new HTTPSAutoDetect($enabled, $header_name, $header_value);
+			$this->assertFalse("Should have raised.");
+		}
+		catch (\InvalidArgumentException $e) {}
+	}
+
+	public function HTTPSAutoDetectValueProvider() {
+		$ret = array();
+		foreach ($this->enabledProvider() as $enabled) {
+			$ret[] = array
+				( $enabled[0], "", ""
+				, $enabled[1]);
+		}
+
+		return $ret;
+	}
+
+	public function enabledProvider() {
+		return array(
+				array(0, true)
+				, array(1, true)
+				, array(4, false)
+				, array(false, false)
+				, array(array(), false)
+				, array("https://localhost/", false)
+			);
+	}
+}
