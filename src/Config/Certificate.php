8bdae4c2d2831a76d5e2aaee7ee2aa9fddd73fe1
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2016-10-18 17:01:11 +0200

    add Certificate config file

---
+++ src/Config/Certificate.php
@@ -0,0 +1,22 @@
+<?php
+/* Copyright (c) 2016 Stefan Hecken <stefan.hecken@concepts-and-training.de>, Extended GPL, see LICENSE */
+
+namespace CaT\InstILIAS\Config;
+
+/**
+ * Configuration for an ILIAS certificate.
+ *
+ * @author Stefan Hecken <stefan.hecken@concepts-and-training.de>
+ *
+ * @method string enable()
+ */
+class Certificate extends Base {
+	/**
+	 * @inheritdocs
+	 */
+	public static function fields() {
+		return array
+			( "enable"	=> array("int", false)
+			);
+	}
+}
