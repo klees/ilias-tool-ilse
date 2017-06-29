08a8704ba0eefa376936357a590c094c7c46b1ea
Daniel Weise <daniel.weise@concepts-and-training.de>
2017-06-29 15:07:09 +0200

    extend the reg expression for allowing : in url

--- src/Config/DB.php
+++ src/Config/DB.php
@@ -19,7 +19,7 @@ namespace CaT\InstILIAS\Config;
 class DB extends Base {
 
 	const IP_REGEX = "/^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/";
-	const HOST_NAME_REGEX = "/^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$/";
+	const HOST_NAME_REGEX = "/^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])(:\d+)?$/";
 
 	/**
 	 * @inheritdocs
