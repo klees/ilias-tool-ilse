0fb61ac404a659b7997619fcbe7a2c74c1a78329
Daniel Weise <daniel.weise@concepts-and-training.de>
2017-07-24 15:43:54 +0200

    add 'App.php' for initializing stuff

---
+++ src/App.php
@@ -0,0 +1,18 @@
+<?php
+/* Copyright (c) 2017 Daniel Weise <daniel.weise@concepts-and-training.de>, Extended GPL, see LICENSE */
+
+namespace CaT\InstILIAS;
+
+use Symfony\Component\Console\Application;
+
+
+/**
+ * Do the main initializing
+ */
+class App extends Application
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+}
