e614fdb2749353b1e995ac57605db9443e65fafa
Daniel Weise <daniel.weise@concepts-and-training.de>
2017-07-24 15:42:37 +0200

    add new class 'ilias-installer.php' as new entrypoint for the application

---
+++ ilias-installer.php
@@ -0,0 +1,14 @@
+#!/usr/bin/env php
+<?php
+/**
+ * Main entry point for ilias-installer
+ *
+ * @author Daniel Weise 	<daniel.weise@concepts-and-training.de>
+ */
+require_once(__DIR__."/vendor/autoload.php");
+
+// TODO: remove after development
+error_reporting(E_ALL & ~E_STRICT & ~E_DEPRECATED & ~E_NOTICE);
+
+$app = new \CaT\InstILIAS\App();
+$app->run();
