2e1ed43def3528ca02397faa99ee801386f3ed72
Daniel Weise <daniel.weise@concepts-and-training.de>
2017-07-25 11:33:17 +0200

    rename 'ilias-installer.php' to 'ilse.php' - di CommonPathes interface

--- ilse.php
+++ ilse.php
@@ -10,5 +10,6 @@ require_once(__DIR__."/vendor/autoload.php");
 // TODO: remove after development
 error_reporting(E_ALL & ~E_STRICT & ~E_DEPRECATED & ~E_NOTICE);
 
-$app = new \CaT\InstILIAS\App();
+$path = new \CaT\InstILIAS\CommonPathes();
+$app = new \CaT\InstILIAS\App($path);
 $app->run();
