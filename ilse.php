9d2539ec76e4b1f776380422d43bae9ba793b607
Daniel Weise <daniel.weise@concepts-and-training.de>
2017-07-25 13:07:09 +0200

    InstILIAS to Ilse

--- ilse.php
+++ ilse.php
@@ -1,7 +1,7 @@
 #!/usr/bin/env php
 <?php
 /**
- * Main entry point for ilias-installer
+ * Main entry point for Ilse
  *
  * @author Daniel Weise 	<daniel.weise@concepts-and-training.de>
  */
@@ -10,6 +10,6 @@ require_once(__DIR__."/vendor/autoload.php");
 // TODO: remove after development
 error_reporting(E_ALL & ~E_STRICT & ~E_DEPRECATED & ~E_NOTICE);
 
-$path = new \CaT\InstILIAS\CommonPathes();
-$app = new \CaT\InstILIAS\App($path);
+$path = new \CaT\Ilse\CommonPathes();
+$app = new \CaT\Ilse\App($path);
 $app->run();
