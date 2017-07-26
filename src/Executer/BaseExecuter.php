47866b024ee9c600e79232f0cd4a1f37184efa95
daniel4w <daniel4w@gmx.de>
2017-07-26 08:14:15 +0200

    implement constructor to class 'BaseExecuter'

--- src/Executer/BaseExecuter.php
+++ src/Executer/BaseExecuter.php
@@ -8,5 +8,22 @@ namespace CaT\Ilse\Executer;
  */
 abstract class BaseExecuter
 {
+	/**
+	 * Constructor of the BaseExecuter class
+	 *
+	 * @param $string 		$config_name
+	 */
+	public function _construct($config_name)
+	{
+		assert('is_strig($config_name)');
+
+		if(!is_file($config_name)) {
+			throw new Exception("No config file found. (Path: ".$config_name.")");
+		}
+
+		$yaml_string = file_get_contents($config_name);
+		$parser = new \CaT\Ilse\YamlParser();
+		$this->general_config = $parser->read_config($yaml_string, "\\CaT\\Ilse\\Config\\General");
+	}
 
 }
