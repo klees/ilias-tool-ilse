3f665c0792f4c2205bce2c9c601af09a13cafd01
daniel4w <daniel4w@gmx.de>
2017-07-26 08:03:20 +0200

    add new class 'BaseExecuter'

---
+++ src/Executer/BaseExecuter.php
@@ -0,0 +1,12 @@
+<?php
+/* Copyright (c) 2017 Daniel Weise <daniel.weise@concepts-and-training.de>, Extended GPL, see LICENSE */
+
+namespace CaT\Ilse\Executer;
+
+/**
+ * Base class for all executers
+ */
+abstract class BaseExecuter
+{
+
+}
