d6c520ccea3d870122a252b892c8c69ae51b2ba5
Richard Klees <richard.klees@concepts-and-training.de>
2017-09-01 11:45:45 +0200

    added script to build phar

---
+++ build_phar.php
@@ -0,0 +1,33 @@
+#!/usr/bin/env php
+<?php
+
+/* Copyright (c) 2017 Richard Klees <richard.klees@concepts-and-training.de>, Extended GPL, see LICENSE */
+
+$base_dir = __DIR__;
+$build_dir = __DIR__;
+$phar_name = "ilse.phar";
+$phar_path = "$build_dir/$phar_name";
+
+if (file_exists($phar_path)) {
+	unlink($phar_path);
+}
+
+$phar = new Phar
+	( $phar_path
+	, FilesystemIterator::CURRENT_AS_FILEINFO
+		| FilesystemIterator::KEY_AS_FILENAME
+	, $phar_name
+	);
+
+$phar->buildFromDirectory($base_dir);
+
+$phar->setStub(<<<STUB
+#!/usr/bin/env php
+<?php
+Phar::mapPhar();
+include "phar://$phar_name/ilse.php";
+__HALT_COMPILER();
+STUB
+);
+
+chmod($phar_path, 0755);
