327b67f0ee503312a9796e88638bc498e14f0548
Richard Klees <richard.klees@concepts-and-training.de>
2017-09-01 15:14:05 +0200

    removed git tests again. wasn't included in tag...

--- tests/Git/GitWrapperTest.php
+++ tests/Git/GitWrapperTest.php
@@ -9,7 +9,7 @@ require_once(__DIR__."/GitTest.php");
 // TODO: disables this, as this makes travis fail. It is a problem, that we use THIS repo
 // for testing the functionality. We should instead deliver some small test repo for that
 // purpose.
-abstract class GitWrapperTest extends GitTest
+class GitWrapperTest extends GitTest
 {
 	public function getImplementation($target_dir, $remote_url, $repo_name)
 	{
