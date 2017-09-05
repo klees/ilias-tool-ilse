952d6e6a739982dbd7c47b32dc14b161ebe42ce3
Richard Klees <richard.klees@concepts-and-training.de>
2017-09-05 16:37:03 +0200

    Added smoke-test for ilias.php again.

--- tests/SmokeTest.php
+++ tests/SmokeTest.php
@@ -8,8 +8,6 @@ use CaT\Ilse\Aux\TaskLogger;
 use CaT\Ilse\Aux;
 
 class SmokeTest extends PHPUnit_Framework_TestCase {
-	// TODO: currently this does not worl because ilse relies on some config file in the
-	// users home folder, once this dependency is resolved this could be reactivated
 	public function _test_valid_ClientConfig() {
 		include __DIR__."/../ilse.php";
 	}
