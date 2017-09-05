9cbbaf161eacb56412ecee3fe89f1d55381250bf
Richard Klees <richard.klees@concepts-and-training.de>
2017-09-05 16:20:22 +0200

    removed lax error reporting (from ilse, not ILIAS)

--- ilse.php
+++ ilse.php
@@ -6,9 +6,6 @@
  */
 require_once(__DIR__."/vendor/autoload.php");
 
-
-error_reporting(E_ALL & ~E_STRICT & ~E_DEPRECATED & ~E_NOTICE && ~E_WARNING);
-
 $app 		= new \CaT\Ilse\App\App();
 
 // Do not run in unit testing context
