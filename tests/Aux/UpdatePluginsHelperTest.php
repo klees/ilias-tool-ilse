38cc31c2ab08269f72b8a6af25c5613a72579b70
daniwe4 <daniel.weise@concepts-and-training.de>
2017-10-18 11:59:25 +0200

    Ilse: initialize test object in setUp

--- tests/Aux/UpdatePluginsHelperTest.php
+++ tests/Aux/UpdatePluginsHelperTest.php
@@ -2,24 +2,41 @@
 
 /* Copyright (c) 2017 Daniel Weise <daniel.weise@concepts-and-training.de>, Extended GPL, see LICENSE */
 
-use \CaT\Ilse\Action\UpdatePluginsDirectory;
-use \CaT\Ilse\Aux\UpdatePluginsHelper;
-use \CaT\Ilse\Action\UpdatePlugins;
-use \CaT\Ilse\Config;
-use \CaT\Ilse\IliasReleaseConfigurator;
-use \CaT\Ilse\Aux\TaskLogger;
-use CaT\Ilse\Aux\Git;
-use CaT\Ilse\Aux;
-
-use CaT\Ilse\Action\UpdatePlugin;
+use PHPUnit\Framework\TestCase;
+use CaT\Ilse\Aux\UpdatePluginsHelper;
+use CaT\Ilse\Config\Server;
+use CaT\Ilse\Config\Plugin;
+use CaT\Ilse\Config\Plugins;
+use CaT\Ilse\Config\Git;
+use CaT\Ilse\Aux\Filesystem;
+use CaT\Ilse\Aux\Yaml;
 
 // If database had it own interface like filesystem, we could
 // drop this and write a proper test instead.
 class UpdatePluginsHelperForTest extends UpdatePluginsHelper{
 }
 
-class UpdatePluginsHelperTest extends PHPUnit_Framework_TestCase
+class UpdatePluginsHelperTest extends TestCase
 {
+	public function setUp()
+	{
+		$path = "test/dummy";
+		$url = "https://my_plugin";
+
+		$server = new Server("http://ilias.de", "/var/www/html/ilias", "Europe/Berlin");
+		$git = new Git($url, "master", "5355");
+		$plugin = new Plugin($path, $git);
+		$plugins = new Plugins($path, array($plugin));
+		$filesystem = $this->createMock("CaT\Ilse\Aux\Filesystem");
+		$parser = $this->createMock("CaT\Ilse\Aux\Yaml");
+
+		$this->object = new UpdatePluginsHelperForTest(
+			$server,
+			$plugins,
+			$filesystem,
+			$parser
+			);
+	}
 	public function test_perform()
 	{
 	}
