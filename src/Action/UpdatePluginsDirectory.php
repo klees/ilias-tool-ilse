5e556cfea1e845998d3dd15e8f21b06519214ac8
daniwe4 <daniel.weise@concepts-and-training.de>
2017-12-15 10:40:12 +0000

    remove update plugin helper - refactor code structure

--- src/Action/UpdatePluginsDirectory.php
+++ src/Action/UpdatePluginsDirectory.php
@@ -4,8 +4,6 @@ namespace CaT\Ilse\Action;
 use CaT\Ilse\Aux\Filesystem;
 use CaT\Ilse\Aux\Git\GitFactory;
 use CaT\Ilse\Aux\TaskLogger;
-use CaT\Ilse\Aux\UpdatePluginsHelper;
-use CaT\Ilse\Action\UpdatePlugin;
 
 /**
  * Class UpdatePluginsDirectory
@@ -15,6 +13,8 @@ use CaT\Ilse\Action\UpdatePlugin;
  */
 class UpdatePluginsDirectory implements Action
 {
+	use Plugin;
+
 	const BRANCH = "master";
 
 	/**
@@ -55,13 +55,11 @@ class UpdatePluginsDirectory implements Action
 		Filesystem $filesystem,
 		GitFactory $factory,
 		TaskLogger $task_logger,
-		UpdatePluginsHelper $update_plugins_helper,
 		UpdatePlugins $update_plugins
 	) {
 		$this->filesystem = $filesystem;
 		$this->factory = $factory;
 		$this->task_logger = $task_logger;
-		$this->update_plugins_helper = $update_plugins_helper;
 		$this->update_plugins = $update_plugins;
 	}
 
@@ -85,20 +83,16 @@ class UpdatePluginsDirectory implements Action
 	 */
 	protected function initPluginDir()
 	{
-		if(!$this->filesystem->exists($this->update_plugins_helper->dir()))
-		{
-			$this->task_logger->always("Make plugin directory", function ()
-				{
-					$this->filesystem->makeDirectory($this->update_plugins_helper->dir());
-				});
-		}
-		$this->task_logger->always("Check write permissions", function()
-			{
-				if(!$this->filesystem->isWriteable($this->update_plugins_helper->dir()))
-				{
-					throw new \Exception("No write permissions");
-				}
+		if(!$this->filesystem->exists($this->update_plugins_helper->dir())) {
+			$this->task_logger->always("Make plugin directory", function () {
+				$this->filesystem->makeDirectory($this->update_plugins_helper->dir());
 			});
+		}
+		$this->task_logger->always("Check write permissions", function() {
+			if(!$this->filesystem->isWriteable($this->update_plugins_helper->dir())) {
+					throw new \Exception("No write permissions");
+			}
+		});
 	}
 
 	/**
@@ -110,20 +104,17 @@ class UpdatePluginsDirectory implements Action
 	{
 		$installed_plugins = $this->update_plugins_helper->getInstalledPlugins();
 		$urls = $this->update_plugins_helper->getRepoUrls();
-		$this->task_logger->eventually("Clone new plugins", function () use($urls, $installed_plugins)
-			{
-				foreach ($urls as $url)
-				{
-					$name = $this->update_plugins_helper->getRepoNameFromUrl($url);
-					if(in_array($name, $installed_plugins))
-					{
-						continue;
-					}
-					$this->filesystem->makeDirectory($this->update_plugins_helper->dir()."/".$name);
-					$git = $this->factory->getRepo($this->update_plugins_helper->dir()."/".$name, $url);
-					$this->task_logger->always("clone plugin $name", [$git, "gitClone"]);
+		$this->task_logger->eventually("Clone new plugins", function () use($urls, $installed_plugins) {
+			foreach ($urls as $url) {
+				$name = $this->update_plugins_helper->getRepoNameFromUrl($url);
+				if(in_array($name, $installed_plugins)) {
+					continue;
 				}
-			});
+				$this->filesystem->makeDirectory($this->update_plugins_helper->dir()."/".$name);
+				$git = $this->factory->getRepo($this->update_plugins_helper->dir()."/".$name, $url);
+				$this->task_logger->always("clone plugin $name", [$git, "gitClone"]);
+			}
+		});
 	}
 
 	/**
@@ -134,18 +125,15 @@ class UpdatePluginsDirectory implements Action
 	protected function updatePlugins()
 	{
 		$urls = $this->update_plugins_helper->getRepoUrls();
-		$this->task_logger->eventually("Pull plugins", function () use($urls)
-			{
-				foreach ($urls as $url)
-				{
-					$name = $this->update_plugins_helper->getRepoNameFromUrl($url);
-					$git = $this->factory->getRepo($this->update_plugins_helper->dir().'/'.$name, $url);
-					$this->task_logger->always("pull plugin $name", function() use($git)
-						{
-							$git->gitPull(self::BRANCH);
-						});
-				}
-			});
+		$this->task_logger->eventually("Pull plugins", function () use($urls) {
+			foreach ($urls as $url) {
+				$name = $this->update_plugins_helper->getRepoNameFromUrl($url);
+				$git = $this->factory->getRepo($this->update_plugins_helper->dir().'/'.$name, $url);
+				$this->task_logger->always("pull plugin $name", function() use($git) {
+					$git->gitPull(self::BRANCH);
+				});
+			}
+		});
 	}
 
 	/**
@@ -159,19 +147,16 @@ class UpdatePluginsDirectory implements Action
 		$installed_plugins = $this->update_plugins_helper->getInstalledPlugins();
 		$marked_plugins = $this->update_plugins_helper->getUnlistedPlugins($installed_plugins, $urls);
 
-		$this->task_logger->eventually("Delete plugins", function () use($marked_plugins)
-			{
-				foreach($marked_plugins as $marked_plugin)
-				{
-					$this->task_logger->always("delete plugin $marked_plugin", function() use($marked_plugin)
-						{
-							$link = $this->update_plugins_helper->getPluginLinkPath($marked_plugin);
-							$this->update_plugins->uninstall($link['name']);
-							$this->filesystem->remove($link['path']."/".$link['name']);
-							$this->filesystem->remove($this->update_plugins_helper->dir()."/".$marked_plugin);
-						});
-				}
-			});
+		$this->task_logger->eventually("Delete plugins", function () use($marked_plugins) {
+			foreach($marked_plugins as $marked_plugin) {
+				$this->task_logger->always("delete plugin $marked_plugin", function() use($marked_plugin) {
+					$link = $this->update_plugins_helper->getPluginLinkPath($marked_plugin);
+					$this->update_plugins->uninstall($link['name']);
+					$this->filesystem->remove($link['path']."/".$link['name']);
+					$this->filesystem->remove($this->update_plugins_helper->dir()."/".$marked_plugin);
+				});
+			}
+		});
 	}
 
 	/**
@@ -182,34 +167,27 @@ class UpdatePluginsDirectory implements Action
 	 */
 	protected function linkPluginsToIlias()
 	{
-		$this->task_logger->eventually("Link plugins", function ()
-			{
-				$installed_plugins = $this->update_plugins_helper->getInstalledPlugins();
-				foreach($installed_plugins as $plugin)
-				{
-					$link = $this->update_plugins_helper->getPluginLinkPath($plugin);
-
-					if(!$this->filesystem->exists($link['path']))
-					{
-						$this->filesystem->makeDirectory($link['path']);
-					}
-					if(!$this->filesystem->isWriteable($link['path']))
-					{
-						throw new \Exception("No write acces to ".$link['path'].".");
-					}
-					$this->task_logger->always("link plugin ".$link['name'], function() use($plugin, $link)
-						{
-							if($this->filesystem->isLink($link['path']."/".$link['name']))
-							{
-								return true;
-							}
-							$this->filesystem->symlink(
-								$this->update_plugins_helper->dir()."/".
-								$plugin, $link['path']."/".
-								$link['name']
-								);
-						});
+		$this->task_logger->eventually("Link plugins", function () {
+			$installed_plugins = $this->update_plugins_helper->getInstalledPlugins();
+			foreach($installed_plugins as $plugin) {
+				$link = $this->update_plugins_helper->getPluginLinkPath($plugin);
+				if(!$this->filesystem->exists($link['path'])) {
+					$this->filesystem->makeDirectory($link['path']);
 				}
-			});
+				if(!$this->filesystem->isWriteable($link['path'])) {
+					throw new \Exception("No write acces to ".$link['path'].".");
+				}
+				$this->task_logger->always("link plugin ".$link['name'], function() use($plugin, $link) {
+					if($this->filesystem->isLink($link['path']."/".$link['name'])) {
+						return true;
+					}
+					$this->filesystem->symlink(
+						$this->update_plugins_helper->dir()."/".
+						$plugin, $link['path']."/".
+						$link['name']
+					);
+				});
+			}
+		});
 	}
 }
