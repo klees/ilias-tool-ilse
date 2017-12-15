138aeabd7d8eb50a03abdc96605268ccab5dc349
daniwe4 <daniel.weise@concepts-and-training.de>
2017-12-15 10:12:58 +0000

    implement PluginInfo class

---
+++ src/Aux/ILIAS/PluginInfo.php
@@ -0,0 +1,138 @@
+<?php
+namespace CaT\Ilse\Aux\ILIAS;
+/**
+ * Class PluginInfo
+ * Holds the data needed for installing an ILIAS plugin.
+ *
+ * @author Daniel Weise <daniel.weise@concepts-and-training.de>
+ * @copyright Extended GPL, see LICENSE
+ */
+class PluginInfo implements PluginInfoReader
+{
+	/**
+	 * @var string
+	 */
+	protected $component_type;
+
+	/**
+	 * @var string
+	 */
+	protected $component_name;
+
+	/**
+	 * @var string
+	 */
+	protected $slot;
+
+	/**
+	 * @var string
+	 */
+	protected $plugin_name;
+
+	/**
+	 * Constructor of the class PluginInfo
+	 */
+	public function __construct(
+		$component_type,
+		$component_name,
+		$slot,
+		$plugin_name
+	) {
+		$this->component_type = $component_type;
+		$this->component_name = $component_name;
+		$this->slot = $slot;
+		$this->plugin_name = $plugin_name;
+	}
+
+	/**
+	 * Get commponent_type
+	 *
+	 * @return string
+	 */
+	public function getComponentType()
+	{
+		return $this->commponent_type;
+	}
+
+	/**
+	 * Set commponent_type with $value
+	 *
+	 * @param string		$value
+	 */
+	public function withComponentType($value)
+	{
+		assert('is_string($value)');
+		$clone = clone $this;
+		$clone->commponent_type = $value;
+		return $clone;
+	}
+
+	/**
+	 * Get component_name
+	 *
+	 * @return string
+	 */
+	public function getComponentName()
+	{
+		return $this->component_name;
+	}
+
+	/**
+	 * Set component_name with $value
+	 *
+	 * @param string		$value
+	 */
+	public function withComponentName($value)
+	{
+		assert('is_string($value)');
+		$clone = clone $this;
+		$clone->component_name = $value;
+		return $clone;
+	}
+
+	/**
+	 * Get slot
+	 *
+	 * @return string
+	 */
+	public function getSlot()
+	{
+		return $this->slot;
+	}
+
+	/**
+	 * Set slot with $value
+	 *
+	 * @param string		$value
+	 */
+	public function withSlot($value)
+	{
+		assert('is_string($value)');
+		$clone = clone $this;
+		$clone->slot = $value;
+		return $clone;
+	}
+
+	/**
+	 * Get plugin_name
+	 *
+	 * @return string
+	 */
+	public function getPluginName()
+	{
+		return $this->plugin_name;
+	}
+	
+	/**
+	 * Set plugin_name with $value
+	 *
+	 * @param string		$value
+	 */
+	public function withPluginName($value)
+	{
+		assert('is_string($value)');
+		$clone = clone $this;
+		$clone->plugin_name = $value;
+		return $clone;
+	}
+}
