d07e963224a3f06b641ae0c57da40510d0b342c7
daniwe4 <daniel.weise@concepts-and-training.de>
2018-01-03 10:24:45 +0000

    first update of tests

--- tests/Actions/UpdatePluginsDirectoryTest.php
+++ tests/Actions/UpdatePluginsDirectoryTest.php
@@ -9,6 +9,7 @@ use \CaT\Ilse\Config;
 use \CaT\Ilse\IliasReleaseConfigurator;
 use \CaT\Ilse\Aux\TaskLogger;
 use CaT\Ilse\Aux\Git;
+use CaT\Ilse\Aux\ILIAS;
 use CaT\Ilse\Aux;
 
 use CaT\Ilse\Action\UpdatePlugin;
@@ -23,7 +24,7 @@ class UpdatePluginsDirectoryTest extends PHPUnit_Framework_TestCase
 	public function test_perform()
 	{
 		$url = "https://my_plugin";
-		$path = "test/dummy";
+		$path = "/home/vagrant/dummy";
 		$name = "my_plugin";
 		$yaml = "---
 ComponentType: Services
@@ -35,9 +36,8 @@ SlotId: evhk";
 		$git_wrapper = $this->createMock(Git\GitWrapper::class);
 		$filesystem = $this->createMock("CaT\Ilse\Aux\Filesystem");
 		$task_logger = $this->createMock(TaskLogger::class);
-		$update_plugins_helper = $this->createMock(UpdatePluginsHelper::class);
 		$update_plugins = $this->createMock(UpdatePlugins::class);
-		$parser = $this->createMock("CaT\Ilse\Aux\Yaml");
+		$plugin_info_reader_factory = $this->createMock(ILIAS\PluginInfoReaderFactory::class);
 
 		$git = new Config\Git($url, "master", "5355");
 		$server = new Config\Server("http://ilias.de", "/var/www/html/ilias", "Europe/Berlin");
@@ -45,37 +45,44 @@ SlotId: evhk";
 		$plugins = new Config\Plugins($path, array($plugin));
 
 		$action = new UpdatePluginsDirectoryForTest(
+			$server,
+			$plugins,
 			$filesystem,
 			$git_factory,
 			$task_logger,
-			$update_plugins_helper,
-			$update_plugins,
-			$parser
+			$plugin_info_reader_factory,
+			$update_plugins
 			);
 
 		$filesystem
 			->expects($this->any())
 			->method("makeDirectory")
-			->willReturn($path);
-		$filesystem
-			->expects($this->any())
-			->method("exists")
-			->willReturn(true);
+			->will($this->onConsecutiveCalls(
+				array(
+					$path,
+					$path.'/'.$name,
+					$path
+				)));
+
+		// $filesystem
+		// 	->expects($this->any())
+		// 	->method("exists")
+		// 	->willReturn(true);
 		$filesystem
 			->expects($this->any())
 			->method("isWriteable")
 			->willReturn(true);
-		$filesystem
-			->expects($this->any())
-			->method("getSubdirectories")
-			->willReturn(array("test", "test2"));
-		$filesystem
-			->expects($this->any())
-			->method("read")
-			->willReturn($yaml);
+		// $filesystem
+		// 	->expects($this->any())
+		// 	->method("getSubdirectories")
+		// 	->willReturn(array("test", "test2"));
+		// $filesystem
+		// 	->expects($this->any())
+		// 	->method("read")
+		// 	->willReturn($yaml);
 
 		$git_factory
-			->expects($this->at(0))
+			->expects($this->any())
 			->method("getRepo")
 			->with($path."/".$name, $url, false)
 			->willReturn($git_wrapper);
@@ -93,34 +100,13 @@ SlotId: evhk";
 				$c();
 			}));
 
-		$git_wrapper
-			->expects($this->any())
-			->method("gitClone");
+		// $git_wrapper
+		// 	->expects($this->any())
+		// 	->method("gitClone");
 
-		$update_plugins_helper
-			->expects($this->any())
-			->method("getRepoUrls")
-			->willReturn(array($url));
-		$update_plugins_helper
-			->expects($this->any())
-			->method("getRepoNameFromUrl")
-			->willReturn($name);
-		$update_plugins_helper
-			->expects($this->any())
-			->method("getInstalledPlugins")
-			->willReturn(array($name, $name));
-		$update_plugins_helper
-			->expects($this->any())
-			->method("getUnlistedPlugins")
-			->willReturn(array("test", "noch"));
-		$update_plugins_helper
-			->expects($this->any())
-			->method("dir")
-			->willReturn($path);
-
-		$update_plugins
-			->expects($this->any())
-			->method("uninstall");
+		// $update_plugins
+		// 	->expects($this->any())
+		// 	->method("uninstall");
 
 		$action->perform();
 	}
